720f03bc325a42b523ffb4d5b3ad0519d2b205bf
Breanna Young <w0459556@campus.nscc.ca>
2023-12-07 13:11:54 -0400

    insert intos

--- sqlscript.sql
+++ sqlscript.sql
@@ -11,7 +11,7 @@ CREATE TABLE [Adopters]( -- adopters table
 	adopt_LName varchar(50) NOT NULL, -- LAST NAME, cant be null
 	adopt_Addr1 varchar(50) NOT NULL, -- address 1, cant be null
 	adopt_Addr2 varchar(50) NOT NULL, -- address 2, cant be null
-  adopt_Email varchar(50) NOT NULL, -- email address, cant be null
+    adopt_Email varchar(50) NOT NULL, -- email address, cant be null
 	adopt_PhoneNum varchar(50) NOT NULL, -- phone number, cant be null
 	news_ID_FK int NOT NULL, -- news foreign key, cant be null
 	adopt_Em_PhoneNum varchar(50) NOT NULL, -- emergency contact phone number, cant be null
@@ -43,8 +43,7 @@ CREATE TABLE [Cat]( -- adopters table
 );
 
 CREATE TABLE [Shelters]( -- adopters table
-	shelter_ID int NOT NULL IDENTITY(1,1) PRIMARY KEY, -- the ID, which is the Primary Key, cant be null. duh
-    shelter_Name varchar(50) NOT NULL, -- name of the shelter
+    shelter_Name varchar(50) NOT NULL PRIMARY KEY,, -- name of the shelter
     shelter_Address varchar(50) NOT NULL, -- address of the shelter, only one- change if necessary
 );
 
